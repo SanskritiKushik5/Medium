f1236ca43eed650252fc83f282c3095cdf77befc
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-08-18 16:36:54 +0530

    ToDo App

---
+++ todo/TodoApp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import home, add_todo, delete_todo
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('add_todo', add_todo, name='add_todo'),
+    path('delete_todo/<int:todo_id>', delete_todo, name='delete_todo'),
+]
---
+++ todo/TodoApp/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from .models import ToDo
+
+# Create your views here.
+def home(request):
+    template_name = 'index.html'
+    todo_list = ToDo.objects.all() # Retrieves queryset
+    context = {'todo_list': todo_list}
+    return render(request, template_name, context=context)
+
+@csrf_exempt
+def add_todo(request):
+    if request.method == "POST":
+        todo_text = request.POST["todo_text"]
+        ToDo.objects.create(
+            todo_text=todo_text,
+        ) # Saving a Todo in the model ToDo
+    
+    return redirect('home')
+
+@csrf_exempt
+def delete_todo(request, todo_id):
+    if request.method == 'POST':
+        todo_obj = ToDo.objects.get(pk=todo_id) # Retrieving an object with id that is taken from the url
+        todo_obj.delete() # Deleting the retrieved object
+
+    return redirect('home')
